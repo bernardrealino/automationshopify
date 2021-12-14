f0d947830aa914d992c04b4cd828325d02cc0f11
Bernard <bernardinus.r.w.k.j@polytron.co.id>
2021-12-14 08:56:07 +0700

    - proceed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 #%%
 import pandas as pd
@@ -30,11 +31,59 @@ def install(data_no):
         pass
     element.click()
 
-    driver.find_element_by_name("signup[email]").send_keys(df["email"][data_no])
-    driver.find_element_by_name("signup[password]").send_keys(df["password"][data_no])
-    driver.find_element_by_name("signup[shop_name]").send_keys(df["name"][data_no])
+    # driver.find_element_by_name("signup[email]").send_keys(df["email"][data_no])
+    # driver.find_element_by_name("signup[password]").send_keys(df["password"][data_no])
+    # driver.find_element_by_name("signup[shop_name]").send_keys(df["name"][data_no])
+    x = 28
+    driver.find_element_by_name("signup[email]").send_keys(f"john99{str(x)}@gmail.com")
+    driver.find_element_by_name("signup[password]").send_keys("john123")
+    driver.find_element_by_name("signup[shop_name]").send_keys(f"john99{str(x)}")
     driver.find_element_by_xpath('//*[@id="SignupForm_modal"]/div[6]/button').click()
-    
+    driver.implicitly_wait(10) # seconds
+    driver.find_element_by_xpath('//*[@id="AppFrameMain"]/div/div/div/div[1]/div/footer/div/div[2]/button').click()
+
+    country = driver.find_element_by_name('account_setup[country]')
+    select_country = Select(country)
+    fname = driver.find_element_by_name('account_setup[firstName]')
+    lname = driver.find_element_by_name('account_setup[lastName]')
+    city = driver.find_element_by_name('account_setup[city]')
+    address = driver.find_element_by_name('account_setup[address1]')
+    province = driver.find_element_by_name('province')
+    select_province = Select(province)
+    zip = driver.find_element_by_name('account_setup[zip]')
+    phone = driver.find_element_by_name('account_setup[phone]')
+
+    select_country.select_by_visible_text("Indonesia")
+    fname.send_keys("hello")
+    lname.send_keys("there")
+    address.send_keys("this street")
+    city.send_keys("luxemburg")
+    select_province.select_by_visible_text("Lampung")
+    zip.send_keys("35111")
+    phone.send_keys("+62852271289")
+
+    #proceed
+    driver.find_element_by_xpath('//*[@id="AppFrameMain"]/div/div/div/div[2]/div/footer/div[1]/div[2]/button').click()
+    driver.implicitly_wait(10) # seconds
+    driver.get('https://apps.shopify.com/cooki-gdpr?surface_detail=cooki&surface_inter_position=1&surface_intra_position=15&surface_type=search')
+    driver.implicitly_wait(10) # seconds
+    #add app
+    # driver.find_element_by_xpath('//*[@id="Main"]/section[1]/div/div/div/div[6]/form/input[2]')
+    # driver.implicitly_wait(5) # seconds
+    # driver.find_element_by_id('proceed_cta').click()
+
+    # driver.find_elements_by_link_text("Apps").click
+    # driver.find_element_by_xpath('//*[@id="AppFrameMain"]/div/div/div[1]/div/div[2]/div/a').click()
+    # driver.implicitly_wait(10) # seconds
+    # driver.find_element_by_name('q').send_keys("cooki" + Keys.ENTER)
+    # driver.implicitly_wait(5) # seconds
+    # driver.find_element_by_xpath('//*[@id="SearchResultsListings"]/div[15]/div/a').click()
+    # driver.find_element_by_xpath('//*[@id="Main"]/section[1]/div/div/div/div[6]/form/input[2]').click()
+    # driver.implicitly_wait(5) # seconds
+    # driver.find_element_by_id('proceed_cta').click()
+
+
     # time.sleep(5)
     # driver.quit()
 install(0)
+# %%
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+    # driver.find_elements_by_link_text("Apps").click
+    # driver.find_element_by_xpath('//*[@id="AppFrameMain"]/div/div/div[1]/div/div[2]/div/a').click()
+    # driver.implicitly_wait(10) # seconds
+    # driver.find_element_by_name('q').send_keys("cooki" + Keys.ENTER)
+    # driver.implicitly_wait(5) # seconds
+    # driver.find_element_by_xpath('//*[@id="SearchResultsListings"]/div[15]/div/a').click()
+    # driver.find_element_by_xpath('//*[@id="Main"]/section[1]/div/div/div/div[6]/form/input[2]').click()
+    # driver.implicitly_wait(5) # seconds
+    # driver.find_element_by_id('proceed_
